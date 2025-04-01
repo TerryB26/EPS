8545cb26a295fa13eee7252e6735cf87940bc5a0
Terry Ndlovu <ndlovu_terry@yahoo.com>
2025-04-01 13:15:18 +0200

    Formbuttons click fic

--- components/General/FormButtons.jsx
+++ components/General/FormButtons.jsx
@@ -23,12 +23,15 @@ const FormButtons = ({
   };
 
   useEffect(() => {
-    const handleClickOutside = () => {
+    const handleClickOutside = (event) => {
+      if (event.target.closest(".form-buttons-container")) {
+        return;
+      }
       if (showButtons) {
         setShowButtons(false);
       }
     };
-
+  
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
@@ -36,120 +39,24 @@ const FormButtons = ({
   }, [showButtons]);
 
   return (
-    <Grid container spacing={2} sx={{ pt: 2 }}>
-      <Grid
-        item
-        xs={12}
-        sm={12}
-        sx={{
-          display: "flex",
-          justifyContent: "start",
-          alignItems: "center",
-        }}
-      >
-        {showButtons ? (
-          <>
-            {showStepperBack && (
-              <Button
-                variant="text"
-                color="inherit"
-                disabled={activeStep === 0}
-                sx={{
-                  backgroundColor: "transparent",
-                  color: "black",
-                  boxShadow: "none",
-                  border: "none",
-                  "&:hover": {
-                    backgroundColor: "transparent",
-                    fontWeight: "bold",
-                    border: "1px solid gray",
-                    "& svg": {
-                      fontWeight: "bold",
-                    },
-                  },
-                }}
-                startIcon={
-                  <MdSettingsBackupRestore style={{ marginBottom: "-2px" }} />
-                }
-                onClick={handleBack}
-              >
-                Back
-              </Button>
-            )}
-            <Button
-              variant="text"
-              color="inherit"
-              sx={{
-                backgroundColor: "transparent",
-                color: "black",
-                boxShadow: "none",
-                border: "none",
-                "&:hover": {
-                  backgroundColor: "transparent",
-                  fontWeight: "bold",
-                  border: "1px solid red",
-                  "& svg": {
-                    fontWeight: "bold",
-                  },
-                },
-              }}
-              startIcon={
-                <IoMdCloseCircleOutline style={{ marginBottom: "-2px" }} />
-              }
-              onClick={handleClose}
-            >
-              close
-            </Button>
-
-            <Button
-              variant="text"
-              color="inherit"
-              sx={{
-                backgroundColor: "transparent",
-                color: "black",
-                boxShadow: "none",
-                border: "none",
-                "&:hover": {
-                  backgroundColor: "transparent",
-                  fontWeight: "bold",
-                  border: "1px solid black",
-                  "& svg": {
-                    fontWeight: "bold",
-                  },
-                },
-              }}
-              startIcon={<PiBroomLight style={{ marginBottom: "0px" }} />}
-              onClick={handleClearForm}
-            >
-              Clear Form
-            </Button>
-
-            <LoadingButton
-              type="submit"
-              loading={submitting}
-              variant="text"
-              sx={{
-                backgroundColor: "transparent",
-                color: "black",
-                boxShadow: "none",
-                border: "none",
-                "&:hover": {
-                  backgroundColor: "#83CED8",
-                  fontWeight: "bold",
-                  "& svg": {
-                    fontWeight: "bold",
-                  },
-                },
-              }}
-              endIcon={<VscSend style={{ marginBottom: "0px" }} />}
-            >
-              Submit
-            </LoadingButton>
-          </>
-        ) : (
+  <Grid
+    item
+    xs={12}
+    sm={12}
+    className="form-buttons-container" // Add this class
+    sx={{
+      display: "flex",
+      justifyContent: "start",
+      alignItems: "center",
+    }}
+  >
+    {showButtons ? (
+      <>
+        {showStepperBack && (
           <Button
             variant="text"
             color="inherit"
+            disabled={activeStep === 0}
             sx={{
               backgroundColor: "transparent",
               color: "black",
@@ -158,18 +65,113 @@ const FormButtons = ({
               "&:hover": {
                 backgroundColor: "transparent",
                 fontWeight: "bold",
+                border: "1px solid gray",
                 "& svg": {
                   fontWeight: "bold",
                 },
               },
             }}
-            onClick={handleDotsClick}
+            startIcon={
+              <MdSettingsBackupRestore style={{ marginBottom: "-2px" }} />
+            }
+            onClick={handleBack}
           >
-            <MoreHorizIcon style={{ marginBottom: "-2px" }} />
+            Back
           </Button>
         )}
-      </Grid>
-    </Grid>
+        <Button
+          variant="text"
+          color="inherit"
+          sx={{
+            backgroundColor: "transparent",
+            color: "black",
+            boxShadow: "none",
+            border: "none",
+            "&:hover": {
+              backgroundColor: "transparent",
+              fontWeight: "bold",
+              border: "1px solid red",
+              "& svg": {
+                fontWeight: "bold",
+              },
+            },
+          }}
+          startIcon={
+            <IoMdCloseCircleOutline style={{ marginBottom: "-2px" }} />
+          }
+          onClick={handleClose}
+        >
+          close
+        </Button>
+
+        <Button
+          variant="text"
+          color="inherit"
+          sx={{
+            backgroundColor: "transparent",
+            color: "black",
+            boxShadow: "none",
+            border: "none",
+            "&:hover": {
+              backgroundColor: "transparent",
+              fontWeight: "bold",
+              border: "1px solid black",
+              "& svg": {
+                fontWeight: "bold",
+              },
+            },
+          }}
+          startIcon={<PiBroomLight style={{ marginBottom: "0px" }} />}
+          onClick={handleClearForm}
+        >
+          Clear Form
+        </Button>
+
+        <LoadingButton
+          type="submit"
+          loading={submitting}
+          variant="text"
+          sx={{
+            backgroundColor: "transparent",
+            color: "black",
+            boxShadow: "none",
+            border: "none",
+            "&:hover": {
+              backgroundColor: "#83CED8",
+              fontWeight: "bold",
+              "& svg": {
+                fontWeight: "bold",
+              },
+            },
+          }}
+          endIcon={<VscSend style={{ marginBottom: "0px" }} />}
+        >
+          Submit
+        </LoadingButton>
+      </>
+    ) : (
+      <Button
+        variant="text"
+        color="inherit"
+        sx={{
+          backgroundColor: "transparent",
+          color: "black",
+          boxShadow: "none",
+          border: "none",
+          "&:hover": {
+            backgroundColor: "transparent",
+            fontWeight: "bold",
+            "& svg": {
+              fontWeight: "bold",
+            },
+          },
+        }}
+        onClick={handleDotsClick}
+      >
+        <MoreHorizIcon style={{ marginBottom: "-2px" }} />
+      </Button>
+    )}
+  </Grid>
   );
 };
 
